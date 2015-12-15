541964c13e6c47e90f560bedb47ae9e6dff17876
ArthixFR <julien.thomas018@live.fr>
2015-12-15 12:01:41 +0100

    Structure du site + animation bg

---
+++ include/slider.php
@@ -0,0 +1,68 @@
+<div id="choose_server">   
+    <ul id="buttons"> 
+        <li><a href="#" id="tab1" onclick="selectTab(1); return false;" onfocus="blur();"><span class="valign">LibertyStory</span></a></li>
+        <li><a href="#" id="tab2" onclick="selectTab(2); return false;" onfocus="blur();"><span class="valign">Pokedia</span></a></li>
+        <li><a href="#" id="tab3" onclick="selectTab(3); return false;" onfocus="blur();"><span class="valign">UprisingZ</span></a></li>
+        <li><a href="#" id="tab4" onclick="selectTab(4); return false;" onfocus="blur();"><span class="valign">Soon</span></a></li>
+    </ul>
+
+    <div id="box1" class="infobox"> 
+        <p>such as:</p>
+        <ul>
+            <li>N</li>
+            <li>M</li>
+            <li>A</li>
+            <li>F</li>
+            <li>A</li>
+        </ul>
+    </div>
+
+    <div id="box2" class="infobox">  
+        <p>box 2 <BR/><BR/><BR/><BR/><BR/><BR/></p>
+    </div>
+
+    <div id="box3" class="infobox">
+        <ul id="tabs">
+            <li class="active">Tab 1</li>
+            <li>Tab 2</li>
+            <li>Tab 3</li>
+            <li>Tab 4</li>
+        </ul>
+        <ul id="tab">
+            <li class="active">
+                <h2>This is the first tab</h2>
+            </li>
+            <li>
+                <h2>This is the second tab</h2>
+            </li>
+            <li>
+                <h2>Tab number three wee hee</h2>
+            </li>
+            <li>
+                <h2>Fourth tab not bad</h2>
+            </li>
+        </ul>
+    </div>
+
+    <div id="box4" class="infobox">
+        <p>this is a test. this is a test. Et ta mère le test ? </p>
+        <p>this is a test. this is a test. Et ta mère le test ? </p>
+        <p>this is a test. this is a test. Et ta mère le test ? </p>
+        <p>this is a test. this is a test. Et ta mère le test ? </p>
+        <p>this is a test. this is a test. Et ta mère le test ? </p>
+        <p>this is a test. tfdrghdgdgdrgd. Et ta mère le test ? </p>
+        <p>this is a test. this is a test. Et ta mère le test ? </p>
+        <p>this is a test. this is a test. Et ta mère le test ? </p>
+        <p>this is a test. this is a test. Et ta mère le test ? </p>
+        <p>this is a test. this is a test. Et ta mère le test ? </p>
+        <p>this is a test. this is a test. Et ta mère le test ? </p>
+        <p>this is a test. this is a test. Et ta mère le test ? </p>
+        <p>this is a test. this is a test. Et ta mère le test ? </p>
+        <ul>
+            <li>H</li>
+            <li>W</li>
+            <li>E</li>
+            <li>S</li>
+        </ul>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,28 +1,47 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Accueil</title>
-	<meta charset="utf-8" />
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
-	<body>
-		<header class="banniere"> <!-- BANNIERE DU HAUT DE PAGE (HEADER) SUR TOUT LA LARGEUR -->
-			<div class="banniere_image"></div> <!-- IMAGE DE LA BANNIERE -->
-		</header>
+    <head>
+        <title>Accueil</title>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+        <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+        <script src="js/tabs.js"></script>    
+        <script language="JavaScript" type="text/javascript">
+            function selectTab(num) {
+              for (var i=1; i <= 4; i++) {
+                document.getElementById("tab" + i).className = "";
+                document.getElementById("box" + i).className = "infobox";
+              }
+              document.getElementById("tab" + num).className = "selected";
+              document.getElementById("box" + num).className = "infobox enabled";
+            }
+        </script>
+    </head>
+    <body onload="selectTab(3);">
+        <div class="pour_le_footer">
+            <header class="banniere"> <!-- BANNIERE DU HAUT DE PAGE (HEADER) SUR TOUT LA LARGEUR -->
+                <div class="banniere_img">
+                </div> <!-- IMAGE DE LA BANNIERE -->
+            </header>
 
-		<section class="contenu_page"> <!-- BODY DU SITE AVEC LE CONTENU DE LA PAGE (MENU ET SLIDER INFO SERVEUR) -->
+            <section class="contenu_page"> <!-- BODY DU SITE AVEC LE CONTENU DE LA PAGE (MENU ET SLIDER INFO SERVEUR) -->
 
-				<div class="menu">	<!-- MENU CONNEXION/INSCRIPITION ET ESPACE MEMBRE -->
-				</div>
+                <div class="menu">  <!-- MENU CONNEXION/INSCRIPITION ET ESPACE MEMBRE -->
+                </div>
 
-				<div class="contenu_serveur"> <!-- SLIDER SERVEUR EN MILIEU DE PAGE DISPLAY INFO SERVEUR -->
-				</div>	
+                <div class="contenu_serveur"> <!-- SLIDER SERVEUR EN MILIEU DE PAGE DISPLAY INFO SERVEUR -->
+                <?php include'include/slider.php'; ?>
+                </div>
 
-		</section>
+            </section>
+        </div>
 
-		<footer class="footer"> <!-- FOOTER DU SITE -->
-			<div class="copyright">Copyright Multigaming 2015 - 2016</div> <!-- TEXTE FOOTER DU SITE -->
-		</footer>
-
-	</body>
+    <footer class="footer"> <!-- FOOTER DU SITE -->
+        <div class="copyright">Copyright Multigaming 2015 - 2016</div> <!-- TEXTE FOOTER DU SITE -->
+    </footer>
+    <video autoplay id="bgvid" loop>
+        <source src="img/1.mp4" type="video/mp4">
+    </video>
+    </body>
 </html>
